b5eb8f21a6a21c14fa7c8fc0d99b37611d170eca
Sergey Bykov <s.bykov@velestar.com>
2011-03-30 01:26:01 +0300

    3.1 version

---
+++ Commons/RadioFramework/MyClass.h
@@ -0,0 +1,16 @@
+//
+//  MyClass.h
+//  RadioFramework
+//
+//  Created by Beherith on 28.03.11.
+//  Copyright 2011 Beherith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MyClass : NSObject {
+    
+}
+
+@end
---
+++ Commons/RadioFramework/MyClass.m
@@ -0,0 +1,14 @@
+//
+//  MyClass.m
+//  RadioFramework
+//
+//  Created by Beherith on 28.03.11.
+//  Copyright 2011 Beherith. All rights reserved.
+//
+
+#import "MyClass.h"
+
+
+@implementation MyClass
+
+@end
